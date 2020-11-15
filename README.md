4bd447791dd180773fc63a8a8868d879140d82d2
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2020-11-16 04:57:07 +0900

    file name change

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 # printf
 
-`ft_printf.c`に２時間内にprintfのコードを書いてください。
+`your_code.c`に２時間内にprintfのコードを書いてください。
 
 書き終えたら```bash test.sh```と打つと合っているか間違っているかが分かります。
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 cp ./test/printf.c ./test/ft_printf.c
 sed -i -e "s/printf/ft_printf/" ./test/ft_printf.c
 
-gcc -fsanitize=address test/ft_printf.c ft_printf.c -o ft_printf -Wall -Wextra -Werror -lm
+gcc -fsanitize=address test/ft_printf.c your_code.c -o ft_printf -Wall -Wextra -Werror -lm
 gcc -fsanitize=address test/printf.c -o original
 ./ft_printf > ft_printf.txt
 ./original > printf.txt
