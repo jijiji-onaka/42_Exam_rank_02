1d0db103b7e8fa2bc281faa2b04c8b13497b412d
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2020-11-17 19:41:54 +0900

    change name

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
-cp ./test/printf.c ./test/ft_printf.c
-sed -i -e "s/printf/ft_printf/" ./test/ft_printf.c
+cp ./utils/printf.c ./utils/ft_printf.c
+sed -i -e "s/printf/ft_printf/" ./utils/ft_printf.c
 
-gcc -fsanitize=address test/ft_printf.c your_code.c -o ft_printf -Wall -Wextra -Werror -lm
-gcc -fsanitize=address test/printf.c -o original
+gcc -fsanitize=address utils/ft_printf.c your_code.c -o ft_printf -Wall -Wextra -Werror -lm
+gcc -fsanitize=address utils/printf.c -o original
 ./ft_printf > ft_printf.txt
 ./original > printf.txt
-rm -f ./test/ft_printf.c-e
+rm -f ./utils/ft_printf.c-e
 gcc -fsanitize=address Dont_touch/main.c Dont_touch/gnl.a -o lll -Wall -Werror -Wextra -lm
 ./lll
-rm -f ft_printf original ft_printf.txt printf.txt lll test/ft_printf.c
+rm -f ft_printf original ft_printf.txt printf.txt lll utils/ft_printf.c
